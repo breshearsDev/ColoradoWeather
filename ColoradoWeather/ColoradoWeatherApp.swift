a8342e6d7f2993666f290d03b054788e299fbab8
Rob Breshears <robbreshears@gmail.com>
2025-09-19 20:18:31 -0600

    Initial Commit

---
+++ ColoradoWeather/ColoradoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoradoWeatherApp.swift
+//  ColoradoWeather
+//
+//  Created by Breshears, Rob - CTC on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ColoradoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColoradoWeatherTests/ColoradoWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColoradoWeatherTests.swift
+//  ColoradoWeatherTests
+//
+//  Created by Breshears, Rob - CTC on 9/19/25.
+//
+
+import Testing
+@testable import ColoradoWeather
+
+struct ColoradoWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
